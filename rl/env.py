057c3374d170a7ea73396dc88f9c28cf87819664
Jens Egholm <jensegholm@protonmail.com>
2021-09-17 16:38:09 +0200

    Added test for food and observation

--- rl/env.py
+++ rl/env.py
@@ -55,25 +55,60 @@ class GridworldEnv(gym.Env):
             ([x.n * self.pixel_scale for x in self.observation_space])
         )
         highest_indices = np.unravel_index(np.argsort(prob, axis=None), prob.shape)
-        self.food = list(zip(highest_indices[0][:self.food_items], highest_indices[1][:self.food_items]))
+        self.food = np.array(
+            list(
+                zip(
+                    highest_indices[0][: self.food_items],
+                    highest_indices[1][: self.food_items],
+                )
+            )
+        )
 
+    def _closest_food(self, position):
+        min_dist = +math.inf
+        min_pos = None
+        for f in self.food:
+            d = np.linalg.norm(f - np.array(position))
+            if d < min_dist:
+                min_dist = d
+                min_pos = f
+        return min_dist, min_pos
+
+    def _observe(self):
+        # Define reward
+        dist, food_pos = self._closest_food(self.state[:2])
+        if dist == 0:
+            self.food.remove(food_pos)  # Delete food
+            reward = 1
+            dist, food_pos = self._closest_food(self.state[:2])
+        else:
+            reward = 0
+
+        # Define angle to food
+        p1 = np.zeros(2)
+        p2 = self.state[:2] - food_pos
+        angle = np.math.atan2(np.linalg.det([p1, p2]), np.dot(p1, p2))
+        return np.array([dist, angle]), reward
 
     def render(self, mode="rgb_array"):
         img = np.zeros((*[x.n * self.pixel_scale for x in self.observation_space], 3))
+        if len(self.food) == 0:
+            return img
+
         # Draw food
         for (x, y) in self.food:
-            self._draw_square(img, x / self.pixel_scale, y / self.pixel_scale, [0, 1, 0])
+            self._draw_square(
+                img, x / self.pixel_scale, y / self.pixel_scale, [0, 1, 0]
+            )
         # Draw agent
         self._draw_square(img, *self.state[:2], [1, 0, 0])
         return img
 
     def reset(self):
         # Init in center pointing north
-        self.state = np.array(
-            [int(x.n / 2) for x in self.observation_space] + [0]
-        )
+        self.state = np.array([int(x.n / 2) for x in self.observation_space] + [0])
         self._distribute_food()
-        return self.state
+        return self._observe()[0]
 
     def step(self, action):
         err_msg = "%r (%s) invalid" % (action, type(action))
@@ -93,13 +128,8 @@ class GridworldEnv(gym.Env):
                 self.state = np.array([*(self.state[:2] + move), angle])
 
         # Define observation
-        observation = np.array([0, 0])
-
-        # Define reward
-        # 0: if not on food
-        # 1: if on food source, delete food
-
-        return observation, 0, False, {}
+        observation, reward = self._observe()
+        return observation, reward, len(self.food) == 0, {}
 
 
 if __name__ == "__main__":
--- rl/test/test_env.py
+++ rl/test/test_env.py
@@ -26,3 +26,24 @@ def test_step_forward():
    w.reset()
    w.step([1, 1])
    assert np.allclose(w.state, np.array([33, 32, 0]))
+
+def test_food_pos():
+   w = GridworldEnv(dt = 0.5)
+   w.reset()
+   w.food = [(32, 31)]
+   assert w._closest_food((32, 32)) == (1, (32, 31))
+
+def test_food_pos_diagonal():
+   w = GridworldEnv(dt = 0.5)
+   w.reset()
+   w.food = [(31, 30), (0, 0)]
+   assert w._closest_food((32, 32)) == (2.23606797749979, (31, 30))
+
+def test_step_observation():
+   w = GridworldEnv(dt = 0.5)
+   w.reset()
+   w.food = [(32, 31)]
+   obs, rew, end, _ = w.step([0, 0])
+   assert np.allclose(obs, np.array([1, 0]))
+   assert rew == 0
+   assert not end
