3685a345553eeedb8424522571d7fd13656b60ee
Jens Egholm <jensegholm@protonmail.com>
2021-09-21 17:12:13 +0200

    Fixed dependencies

--- setup.py
+++ setup.py
@@ -30,11 +30,11 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.0.8",  # Required
+    version="0.0.9",  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description="Reinforcement learning environment for spiking neural network",  # Optional
+    description="Reinforcement learning environment for spiking neural networks",  # Optional
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
     #
@@ -65,7 +65,7 @@ setup(
     author="Jens E. Pedersen",  # Optional
     # This should be a valid email address corresponding to the author listed
     # above.
-    author_email="jeped@kth.se.",  # Optional
+    author_email="jeped@kth.se",  # Optional
     # When your source code is in a subdirectory under the project root, e.g.
     # `src/`, it is necessary to specify the `package_dir` argument.
     # package_dir={"norse_rl": "norse_rl"},  # Optional
